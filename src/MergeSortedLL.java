f88d491960b6a3564731d047a70e5b394d594d3b
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-23 02:40:18 -0700

    merged the sorted lists

--- src/MergeSortedLL.java
+++ src/MergeSortedLL.java
@@ -1,7 +1,7 @@
 
 
 public class MergeSortedLL {
-   class Node{
+   static class Node{
        int data;
        Node next;
        Node(int data){
@@ -10,6 +10,34 @@ public class MergeSortedLL {
        }
    }
    //creating a method to get my resultant nodes as sorted
+    public static Node mergeSortedLL(Node n1, Node n2){
+       if(n1==null){
+           return n2;
+       }
+       if(n2 == null){
+           return n1;
+       }
+       Node head = new Node(0);
+       Node temp = head;
+       while(n1!= null || n2!= null) {
+           if (n2.data > n1.data) {
+               temp.next = n1;
+               temp = temp.next;
+               head = head.next
+           } else {
+               temp.next = n2;
+               temp = temp.next;
+               head = head.next;
+           }
+       }
+           if(n1 != null){
+               temp.next = n1;
+           }
+           if(n2 != null){
+               temp.next = n2;
+           }return head.next;
+       }
+
     public static void main(String[] args) {
 
     }
