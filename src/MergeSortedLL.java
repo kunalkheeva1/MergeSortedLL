36c74018dd482dec036704433d7d47751964e658
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-23 02:31:58 -0700

    node class

--- src/MergeSortedLL.java
+++ src/MergeSortedLL.java
@@ -1,8 +1,15 @@
 
 
 public class MergeSortedLL {
-   
-
+   class Node{
+       int data;
+       Node next;
+       Node(int data){
+           this.data = data;
+           this.next = null;
+       }
+   }
+   //creating a method to get my resultant nodes as sorted
     public static void main(String[] args) {
 
     }
